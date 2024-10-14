96c92f7170474657a60103f722b498a9e4f3ed0f
Alik Khilazhev <a.khilazhev@criteo.com>
2024-10-14 17:36:05 +0200

    Add raycast scripts dir

---
+++ raycast/switch-monitor.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# See full documentation here: https://github.com/raycast/script-commands
+#
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Switch Monitor Input Source
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.icon 🖥️
+# @raycast.packageName Raycast Scripts
+
+DEST_ID=18
+DEST_NAME="Home Mac Mini"
+
+if [ `whoami` == "alikkhilazhev" ]; then
+  DEST_ID=25
+  DEST_NAME="Work Macbook Air"
+fi
+
+echo "Switching monitor input source to $DEST_NAME"
+sleep 2
+/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay set -ddc=$DEST_ID -vcp=inputSelect
