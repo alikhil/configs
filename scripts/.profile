b3c7d0a43353265017476ca61d849c7f6b8d292e
Alik Khilazhev <git@alik.page>
2024-10-23 19:25:36 +0200

    Update configs

---
+++ scripts/.profile
@@ -0,0 +1,2 @@
+# shell script that must be added to .zshrc or .bashrc
+ssh-add
--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,9 @@
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 echo "source $SCRIPT_DIR/.shellrc" >> ~/.zshrc
+echo "source $SCRIPT_DIR/.profile" >> ~/.zprofile
 echo "source $SCRIPT_DIR/.shellrc" >> ~/.bashrc
+echo "source $SCRIPT_DIR/.profile" >> ~/.bash_profile
+
 
 echo ".shellrc has been sourced in ~/.zshrc and ~/.bashrc"
